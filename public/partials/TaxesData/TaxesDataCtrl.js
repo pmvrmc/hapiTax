f515b59431096800c39736da83e48c6f189a1961
Pedro Matos de Carvalho <pedromatosdecarvalho@gmail.com>
2014-09-08 21:34:40 +0100

    using form generator

--- public/partials/TaxesData/TaxesDataCtrl.js
+++ public/partials/TaxesData/TaxesDataCtrl.js
@@ -4,6 +4,9 @@
 
 appControllers.controller('TaxesDataCtrl', ['$scope', 'TaxesDataFactory', function($scope, TaxesDataFactory) {
 
+    //the object that will store the form data aka ng-model
+    $scope.user = {};
+    
 	$scope.submitForm = function(isValid, user) {
 		// check to make sure the form is completely valid
 		if (isValid) {
@@ -53,5 +56,77 @@ appControllers.controller('TaxesDataCtrl', ['$scope', 'TaxesDataFactory', functi
 		}
 		return (digi==nif[8]);
     };
+    
+    
+    $scope.form = [
+            {
+                'formGroupNgClass' : '{ "has-error" : userFieldForm.field.$invalid && !userFieldForm.field.$pristine }',
+                'labelFor' : 'userName',
+                'labelClass' : 'col-md-2 control-label',
+                'labelText' : 'Nome',
+                'inputType' : 'text',
+                'inputId' : 'userName',
+                'inputValidate' : 'empty',
+                'inputName' : 'userName', 
+                'inputPlaceholder' : 'Nome Contribuinte',
+                'inputClass' : 'form-control',
+                'inputNgModel' : 'name',
+                'inputRequired' : true,
+                'otherAttributes' : 'required novalidate',
+                'errorNgShow' : 'userFieldForm.field.$invalid && !userFieldForm.field.$pristine',
+                'errorText' : 'Campo obrigatório'
+            },
+            {
+                'formGroupNgClass' : '{ "has-error" : userFieldForm.field.$invalid && !userFieldForm.field.$pristine }',
+                'labelFor' : 'userTelefone',
+                'labelClass' : 'col-md-2 control-label',
+                'labelText' : 'Telefone',
+                'inputType' : 'text',
+                'inputId' : 'userTelefone',
+                'inputValidate' : '"validateTelefone($value)"',
+                'inputName' : 'telefone', 
+                'inputPlaceholder' : 'Telefone do Contribuinte',
+                'inputClass' : 'form-control',
+                'inputRequired' : false,
+                'inputNgModel' : 'telefone',
+                'otherAttributes' : 'required',
+                'errorNgShow' : 'userFieldForm.field.$invalid && !userFieldForm.field.$pristine',
+                'errorText' : 'Introduza um telefone válido'
+            },
+            {
+                'formGroupNgClass' : '{ "has-error" : userFieldForm.field.$invalid && !userFieldForm.field.$pristine }',
+                'labelFor' : 'userNIF',
+                'labelClass' : 'col-md-2 control-label',
+                'labelText' : 'NIF',
+                'inputType' : 'text',
+                'inputId' : 'userNIF',
+                'inputValidate' : '"validateNif($value)"',
+                'inputName' : 'userNIF', 
+                'inputPlaceholder' : 'NIF',
+                'inputClass' : 'form-control',
+                'inputRequired' : false,
+                'inputNgModel' : 'nif',
+                'otherAttributes' : 'required',
+                'errorNgShow' : 'userFieldForm.field.$invalid && !userFieldForm.field.$pristine',
+                'errorText' : 'Insira um NIF válido'
+            },
+            {
+                'formGroupNgClass' : '{ "has-error" : userFieldForm.field.$invalid && !userFieldForm.field.$pristine }',
+                'labelFor' : 'userEmail',
+                'labelClass' : 'col-md-2 control-label',
+                'labelText' : 'Email',
+                'inputType' : 'email',
+                'inputId' : 'userEmail',
+                'inputValidate' : 'empty',
+                'inputName' : 'userEmail', 
+                'inputPlaceholder' : 'myEmail@emailHost.com',
+                'inputClass' : 'form-control',
+                'inputRequired' : false,
+                'inputNgModel' : 'email',
+                'otherAttributes' : 'required',
+                'errorNgShow' : 'userFieldForm.field.$invalid && !userFieldForm.field.$pristine',
+                'errorText' : 'Email inválido'
+            }
+        ];
    	
 }]);
